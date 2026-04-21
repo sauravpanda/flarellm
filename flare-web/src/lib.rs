4ed2fd67f10b1f2adc899056d2126853076dbef6
Saurav Panda <sgp65@cornell.edu>
2026-04-20 17:36:19 -0700

    cargo fmt

--- flare-web/src/lib.rs
+++ flare-web/src/lib.rs
@@ -34,9 +34,7 @@ use flare_core::tokenizer::{BpeTokenizer, Tokenizer};
 use flare_gpu::WebGpuBackend;
 use flare_loader::gguf::{GgufFile, MetadataValue};
 use flare_loader::tokenizer::GgufVocab;
-use flare_loader::weights::{
-    load_model_weights_with_progress, load_model_weights_with_raw_opt,
-};
+use flare_loader::weights::{load_model_weights_with_progress, load_model_weights_with_raw_opt};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
